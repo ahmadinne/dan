14462e2193f9ac0f1f67d609ee4da3562d8d9bcb
ahmadinne <ahmadinne@proton.me>
2025-11-29 22:16:11 +0700

    feat: update readme, also the install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,15 @@
 #!/usr/bin/env powershell
 $path = "$env:USERPROFILE\Scripts"
+$conf = "$env:USERPROFILE\.config\dan"
+
 if (!(Test-Path $path)) {
 	mkdir $path
+	[System.Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";$path", "User")
+	$env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "User")
 }
-[System.Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";$path", "User")
-$env:PATH = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+
+if (!(Test-Path $conf)) {
+	mkdir $conf
+}
 
 Copy-Item -Path "dan.ps1" -Destination "${path}\dan.ps1" -Force
